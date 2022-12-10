f57a86b5b956eb2f350f1c55f0b52301cd6da315
JoshuaLChung <jchung20@horizon.csueastbay.edu>
2022-12-09 20:44:29 -0800

    Initial Display class

---
+++ src/main/java/Display.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel {
+
+    private static final int WIDTH = 64;
+    private static final int HEIGHT = 32;
+    private int scale = 12;
+    private Graphics2D display;
+    public int[][] pixels = new int[WIDTH][HEIGHT];
+    @Override
+    public void paint(Graphics g) {
+        display = (Graphics2D) g;
+        display.setColor(Color.BLACK);
+        display.fillRect(0, 0, 400, 800);
+    }
+
+    public void clear() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                pixels[x][y] = 0;
+            }
+        }
+        render();
+    }
+
+    public void render() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (pixels[x][y] == 1) display.setColor(Color.BLACK);
+                else display.setColor(Color.WHITE);
+                display.fillRect(x * scale, y * scale, scale, scale);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Chip-8 Emulator");
+        frame.add(new Display());
+        frame.setSize(400, 800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
