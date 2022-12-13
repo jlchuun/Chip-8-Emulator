358b36837f4a347c2f6cb134bc8983169e391410
JoshuaLChung <jchung20@horizon.csueastbay.edu>
2022-12-12 17:16:07 -0800

    Initial decode function with switch cases for all opcodes.

--- src/main/java/CPU.java
+++ src/main/java/CPU.java
@@ -65,4 +65,83 @@ public class CPU {
         return opcode;
     }
 
+    public void decodeOpcode(int opcode) {
+        switch (opcode) {
+            case 0x00E0:    // 00E0: clear screen
+                return;
+            case 0x00EE:    // 00EE: return from subroutine
+                return;
+        }
+        switch (opcode & 0xF000) {
+            case 0x1000:    // 1NNN: jump to NNN
+                return;
+            case 0x2000:    // 2NNN: calls subroutine at NNN
+                return;
+            case 0x3000:    // 3XNN: skip if VX == NN
+                return;
+            case 0x4000:    // 4XNN: skip if VX != NN
+                return;
+            case 0x5000:    // 5XY0: skip if VX == VY
+                return;
+            case 0x6000:    // 6XNN: set VX = NN
+                return;
+            case 0x7000:    // 7XNN: add NN to VX
+                return;
+            case 0x9000:    // 9XY0: skip if VX != VY
+                return;
+            case 0xA000:    // ANNN: set index register to NNN
+                return;
+            case 0xB000:    // BNNN: jump with offset (v0 register)
+                return;
+            case 0xC000:    // CXNN: VX = random number AND NN
+                return;
+            case 0xD000:    // DXYN: display
+                return;
+        }
+        switch (opcode & 0xF00F) {
+            case 0x8000:    // 8XY0: VX = VY
+                return;
+            case 0x8001:    // 8XY1: VX = VX OR VY
+                return;
+            case 0x8002:    // 8XY2: VX = VX AND VY
+                return;
+            case 0x8003:    // 8XY3: VX = VX XOR VY
+                return;
+            case 0x8004:    // 8XY4: VX += VY
+                return;
+            case 0x8005:    // 8XY5: VX -= VY
+                return;
+            case 0x8006:    // 8XY6: shift right
+                return;
+            case 0x8007:    // 8XY7: VX = VY - VX
+                return;
+            case 0x800E:    // 8XYE: shift left
+                return;
+        }
+        switch (opcode & 0XF0FF) {
+            case 0xE09E:    // EX9E: skip if key press == VX
+                return;
+            case 0xE0A1:    // EXA1: skip if key press != VX
+                return;
+            case 0XF007:    // FX07: VX = delay timer
+                return;
+            case 0XF015:    // FX15: delay timer = VX
+                return;
+            case 0XF018:    // FX18: sound timer  = VX
+                return;
+            case 0XF01E:    // FX1E: index register += VX
+                return;
+            case 0XF00A:    // FX0A: blocks and waits for key press and released
+                return;
+            case 0XF029:    // FX29: index register = hexadecimal char in VX
+                return;
+            case 0XF033:    // FX33: binary-coded decimal conversion
+                return;
+            case 0XF055:    // FX55: store/load into memory
+                return;
+            case 0XF065:    // FX65: store/load memory into var registers
+                return;
+        }
+
+    }
 }
